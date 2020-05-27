64010975433a9c49082177fd61096dfecf061e4b
Willcampbellmelb <w.campbell@student.unimelb.edu.au>
2020-05-27 17:13:10 +1000

    feat: created ALEX Trajectory Generator - work in progress

---
+++ src/apps/Application/TrajectoryGenerator/AlexTrajectoryGenerator.h
@@ -0,0 +1,16 @@
+/**
+ * \file AlexTrajectoryGenerator.h
+ * \author William Campbell
+ * \brief Implementation of Base class TrajectoryGenerator for Alex Exoskeleton Robot.
+ *      Used by the University of Melbourne Robotics labs version of the Exoskeleton.
+ * \version 0.1
+ * \date 2020-05-27
+ * 
+ * \copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef ALEXTRAJECTORYGENERATOR_H_INCLUDED
+#define ALEXTRAJECTORYGENERATOR_H_INCLUDED
+
+#endif
