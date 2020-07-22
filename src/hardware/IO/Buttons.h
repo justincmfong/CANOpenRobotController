7e9676376237f03888b3490a1d8d92db209cee43
Jordan <jordanw772@gmail.com>
2020-07-22 12:03:08 +1000

    Feat: Test button path

--- src/hardware/IO/Buttons.h
+++ src/hardware/IO/Buttons.h
@@ -18,7 +18,7 @@ class Buttons : public InputDevice {
     private:
      bool errorButton;
 
-     std::string errorButtonPath = "/sys/class/gpio/gpio67/value";
+     std::string errorButtonPath = "/sys/class/gpio/gpio60/value";
 
      /**
       * \brief check the value of the e-stop button
