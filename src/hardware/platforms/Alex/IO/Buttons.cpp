e87cd41627c450bc34f82bd7cb2795eeb239629e
justinfong-unimelb <fong.j@unimelb.edu.au>
2022-01-25 18:20:23 +1100

    FIX: Fixed Button configuration for NOROBOT

--- src/hardware/platforms/Alex/IO/Buttons.cpp
+++ src/hardware/platforms/Alex/IO/Buttons.cpp
@@ -7,21 +7,22 @@
 
 Buttons::Buttons() {
     spdlog::debug("Button object created");
+#ifndef NOROBOT
     spdlog::info("Init: {}", iolib_init());
     spdlog::info("SetDir: {}", iolib_setdir(8, 10, BBBIO_DIR_IN));
-
+#endif
     errorButton = false;
 };
 Buttons::~Buttons() {
     // Todo: Check if destructor is necessary
 };
 void Buttons::updateInput() {
-
+#ifndef NOROBOT
     if (is_high(8,10))
         errorButton=false;
     else 
         errorButton = true;
-
+#endif
     // std::cout << "Error button value: " << errorButton << std::endl;
 };
 
